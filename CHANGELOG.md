0a01a4fa8bba4926e02dc85013b289c018f2f841
Elco Klingen <disillusion84@gmail.com>
2019-11-23 18:35:23 +0100

    Internal refactoring

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 
 # Changelog
 
+v1.0.3 - Internal refactoring
 v1.0.2 - Bugfix
 v1.0.1 - Options weren't merged correctly
 v1.0.0 - Initial release
--- index.js
+++ index.js
@@ -1,2 +1,47 @@
+// Small vinyl-stream wrapper -aka Gulp plugin- for node-nodifier.
 
-module.exports = require('./src/notifier')
+const DEFAULT_OPTIONS = {
+  title: '',
+  subtitle: '',
+  message: '',
+  icon: '',
+  sound: false,
+  open: false
+}
+
+const DEFAULT_ERROR_OPTIONS = {
+  title: '😱 Oh no!',
+  icon: 'path/to/icon.png',
+  sound: false
+}
+
+const DEFAULT_REGULAR_OPTIONS = {
+  title: '🧐 Pay attention!',
+  icon: 'path/to/icon.png',
+  sound: false
+}
+
+function nodeNotifierWrapper (options = {}, callback = () => {}) {
+  const notifier = require('node-notifier')
+  const reporter = notifier.notify.bind(notifier)
+
+  const report = function (object, options = {}) {
+    if (object instanceof Error) {
+      options = { ...DEFAULT_OPTIONS, ...DEFAULT_ERROR_OPTIONS, ...options }
+    } else {
+      options = { ...DEFAULT_OPTIONS, ...DEFAULT_REGULAR_OPTIONS, ...options }
+    }
+
+    const message = options.message || object.message || object.formatted || object.relativePath || object.fileName || object
+    const result = { ...options, message }
+
+    console.log(`${result.title ? result.title + '\n' : ''}${result.subtitle ? result.subtitle + '\n' : ''}${object.formatted || result.message}`)
+    result.message = (object.formatted || result.message).replace(/\\x1b[@-_][0-?]*[ -/]*[@-~]/gi, '') // Strip ANSI codes
+
+    reporter(result, error => callback(error ? new Error(error) : null))
+  }
+
+  return error => report(error, options, () => callback(error))
+}
+
+module.exports = nodeNotifierWrapper
